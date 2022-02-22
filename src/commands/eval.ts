9661d108120a6e358a64c6a46ac1625a4a6111af
Eliiia <grzesiul1995@gmail.com>
2022-02-22 22:34:28 +0000

    Write up eval command

---
+++ src/commands/eval.ts
@@ -0,0 +1,24 @@
+import { CommandInteraction, MessageEmbed } from "discord.js"
+
+export async function run(interaction: CommandInteraction) {
+    const expression = interaction.options.getString("expression") || ""
+
+    if(expression === "") return interaction.reply("You didn't put in an expression :c")
+
+    let result
+
+    try { 
+        result = await eval(expression).toString()
+    } catch(e) {
+        result = `${e}`
+    }
+
+    if(result === "") result = "<empty response>"
+
+    const embed = new MessageEmbed()
+        .setColor("DEFAULT")
+        .addField("Expression:", `\`\`\`js\n${expression}\`\`\``)
+        .addField("Result:", `\`\`\`js\n${result}\`\`\``)
+
+    return interaction.reply({ embeds: [embed]})
+}
--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -10,10 +10,18 @@ import { token, clientId, mainGuildId, devGuildId } from "./config.json"
 
 const commands = {
     main: [
-        new SlashCommandBuilder().setName("close").setDescription("Closes current ticket."),
+        new SlashCommandBuilder()
+            .setName("close")
+            .setDescription("Closes current ticket."),
     ].map(command => command.toJSON()),
     dev: [
-        new SlashCommandBuilder().setName("eval").setDescription("Runs custom code. (dev-only)"),
+        new SlashCommandBuilder()
+            .setName("eval")
+            .setDescription("Runs custom code. (dev-only)")
+            .addStringOption(option => 
+                option.setName("expression")
+                    .setDescription("The command to run")
+                    .setRequired(true)),
     ].map(command => command.toJSON())
 }
 
