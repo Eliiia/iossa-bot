37e1f451c15b5328d2a238d3a201feaf930fed7c
Eliiia <grzesiul1995@gmail.com>
2022-02-22 19:32:39 +0000

    add register commands script

---
+++ src/registerCommands.ts
@@ -0,0 +1,32 @@
+/* 
+ * if you make any changes, you'll need to manually edit
+ * this file, and run it seperately from the rest of the bot.
+ */
+
+import { SlashCommandBuilder } from "@discordjs/builders"
+import { REST } from "@discordjs/rest"
+import { Routes } from "discord-api-types/v9"
+import { token, clientId, mainGuildId, devGuildId } from "./config.json"
+
+const commands = {
+    main: [
+        new SlashCommandBuilder().setName("close").setDescription("Closes current ticket."),
+    ].map(command => command.toJSON()),
+    dev: [
+        new SlashCommandBuilder().setName("eval").setDescription("Runs custom code. (dev-only)"),
+    ].map(command => command.toJSON())
+}
+
+const rest = new REST({ version: "9" }).setToken(token)
+
+rest.put(
+    Routes.applicationGuildCommands(clientId, mainGuildId),
+    { body: commands.main } 
+)
+
+rest.put(
+    Routes.applicationGuildCommands(clientId, devGuildId),
+    { body: commands.dev }
+)
+
+console.log("Success!")
