256a4577ea42301b333c070ca96f90515668a416
Eliiia <grzesiul1995@gmail.com>
2022-02-23 19:42:51 +0000

    update registerCommands script

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -1,6 +1,9 @@
 /* 
  * if you make any changes, you'll need to manually edit
  * this file, and run it seperately from the rest of the bot.
+ * 
+ * yes, this could be done cleaner.
+ * i just cannot be bothered.
  */
 
 import { SlashCommandBuilder } from "@discordjs/builders"
@@ -8,33 +11,39 @@ import { REST } from "@discordjs/rest"
 import { Routes } from "discord-api-types/v9"
 import { token, clientId, mainGuildId, devGuildId } from "./config.json"
 
-const commands = {
-    main: [
-        new SlashCommandBuilder()
-            .setName("close")
-            .setDescription("Closes current ticket."),
-    ].map(command => command.toJSON()),
-    dev: [
-        new SlashCommandBuilder()
-            .setName("eval")
-            .setDescription("Runs custom code. (dev-only)")
-            .addStringOption(option => 
-                option.setName("expression")
-                    .setDescription("The command to run")
-                    .setRequired(true)),
-    ].map(command => command.toJSON())
-}
+(async () => {
+    const commands = {
+        main: [
+            new SlashCommandBuilder()
+                .setName("close")
+                .setDescription("Closes current ticket."),
+        ].map(command => command.toJSON()),
+        dev: [
+            new SlashCommandBuilder()
+                .setName("eval")
+                .setDescription("Runs custom code. (dev-only)")
+                .addStringOption(option => 
+                    option.setName("expression")
+                        .setDescription("The command to run")
+                        .setRequired(true)),
+        ].map(command => command.toJSON())
+    }
 
-const rest = new REST({ version: "9" }).setToken(token)
+    const rest = new REST({ version: "9" }).setToken(token)
 
-rest.put(
-    Routes.applicationGuildCommands(clientId, mainGuildId),
-    { body: commands.main } 
-)
+    await rest.put(
+        Routes.applicationGuildCommands(clientId, mainGuildId),
+        { body: commands.main } 
+    )
 
-rest.put(
-    Routes.applicationGuildCommands(clientId, devGuildId),
-    { body: commands.dev }
-)
+    console.log("Main commands deployed")
 
-console.log("Success!")
+    await rest.put(
+        Routes.applicationGuildCommands(clientId, devGuildId),
+        { body: [ ...commands.dev, ...commands.main ] }
+    )
+
+    console.log("Developer commands deployed")
+
+    console.log("Success!")
+})()
